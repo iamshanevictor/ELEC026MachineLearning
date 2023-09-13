83e8f82478413b3f69a05c8811609674f7a08394
Shane Victor <112960479+iamshanevictor@users.noreply.github.com>
2023-09-13 19:51:02 +0800

    update

--- 3. Traditional_ML/main.py
+++ 3. Traditional_ML/main.py
@@ -1,59 +1,92 @@
-def unlapi():
+# Function to read affixes from a file
+def read_affixes(file_name):
+    affixes = []
     try:
-        with open('C:\\Users\\Shane Victor\\Desktop\\FilipinoWords\\unlapi.txt') as file:
-            print(file.read())
+        with open(file_name, 'r') as file:
+            affixes = [line.strip() for line in file.readlines()]
+    except FileNotFoundError:
+        print(f"Error: File '{file_name}' not found.")
+    return affixes
 
-    except PermissionError as e:
-        print(e)
-        print("permission error")
-    except FileNotFoundError as e:
-        print(e)
-        print("file not found")
+# Input word from the user
+word = input("Enter a Word: ").lower()
 
-def gitlapi():
-    try:
-        with open('C:\\Users\\Shane Victor\\Desktop\\FilipinoWords\\gitlapi.txt') as file:
-            print(file.read())
+# Read affixes from the file
+prefixes = read_affixes("unlapi.txt")
+infixes = read_affixes("gitlapi.txt")  # Create a file with infixes
+suffixes = read_affixes("hulapi.txt")  # Create a file with suffixes
 
-    except PermissionError as e:
-        print(e)
-        print("permission error")
-    except FileNotFoundError as e:
-        print(e)
-        print("file not found")
+def is_root_word(word):
+    return len(word) == 3 or len(word) == 4
 
-def hulapi():
-    try:
-        with open('C:\\Users\\Shane Victor\\Desktop\\FilipinoWords\\hulapi.txt') as file:
-            print(file.read())
+# Function to determine the type of affixation
+def determine_affixation(word):
+    for prefix in prefixes:
+        if word.startswith(prefix):
+            return "unlapi"
+        
+    for infix in infixes:
+        if infix in word:
+            return "gitlapi"
+    
+    for suffix in suffixes:
+        if word.endswith(suffix):
+            return "hulapi"
+    
+    return "RootWord"
 
-    except PermissionError as e:
-        print(e)
-        print("permission error")
-    except FileNotFoundError as e:
-        print(e)
-        print("file not found")
+# Function to check if a word contains all three types of affixes
+def contains_all_affixes(word):
+    has_unlapi = False
+    has_gitlapi = False
+    has_hulapi = False
+    
+    for prefix in prefixes:
+        if word.startswith(prefix):
+            has_unlapi = True
+    
+    for infix in infixes:
+        if infix in word:
+            has_gitlapi = True
+    
+    for suffix in suffixes:
+        if word.endswith(suffix):
+            has_hulapi = True
+    
+    return has_unlapi and has_gitlapi and has_hulapi
 
-def kiblaan():
-    try:
-        with open('C:\\Users\\Shane Victor\\Desktop\\FilipinoWords\\kiblaan.txt') as file:
-            print(file.read())
+# Function to determine if a word contains both unlapi and hulapi
+def contains_both_unlapi_and_hulapi(word):
+    has_unlapi = False
+    has_hulapi = False
+    
+    for prefix in prefixes:
+        if word.startswith(prefix):
+            has_unlapi = True
+            break
+    
+    for suffix in suffixes:
+        if word.endswith(suffix):
+            has_hulapi = True
+            break
 
-    except PermissionError as e:
-        print(e)
-        print("permission error")
-    except FileNotFoundError as e:
-        print(e)
-        print("file not found")
+# Check if it's a root word or not
+affixation_type = determine_affixation(word)
 
-def laguhan():
-    try:
-        with open('C:\\Users\\Shane Victor\\Desktop\\FilipinoWords\\laguhan.txt') as file:
-            print(file.read())
+if contains_all_affixes(word):
+    print("Not a Root Word!")
+    print("Panlapi:", affixation_type)
 
-    except PermissionError as e:
-        print(e)
-        print("permission error")
-    except FileNotFoundError as e:
-        print(e)
-        print("file not found")
+elif contains_both_unlapi_and_hulapi(word):
+    print("Contains both unlapi and hulapi!")
+    print("Panlapi:", affixation_type)
+
+elif is_root_word(word):
+    print("A root word!")
+
+elif affixation_type == "RootWord":
+    print("A root word!")
+
+else:
+    print("Not a Root Word!")
+    print("Panlapi:", affixation_type)
