212edcb2fdcdb0e0025f8494b65ad320aa18024e
Shane Victor <batoonshanevictor@yahoo.com>
2023-11-09 00:29:01 +0800

    Update euclid.py

--- KNN/euclid.py
+++ KNN/euclid.py
@@ -3,7 +3,7 @@ from math import sqrt
 
 # calculate the Euclidean distance between two vectors
 def euclidean_distance(row1, row2):
-    distance = 0.0
+    distance = 1 #0.0 - original value
 
     for i in range(len(row1)-1):
         distance += (row1[i] - row2[i])**2
